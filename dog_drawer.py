08fc0f17826d12eabb4aa0b0473e48850469a855
Your Name <you@example.com>
2020-01-15 17:58:19 -0800

    Finished cat. Dog in progress

--- dog_drawer.py
+++ dog_drawer.py
@@ -12,7 +12,7 @@ class Cat:
     center_y = None
     color = None
 
-    def __init__(self, center_point_x, center_point_y, color):
+    def __init__(self, center_point_x, center_point_y, color=(158, 102, 41)):
         self.center_x = center_point_x
         self.center_y = center_point_y
         self.color = color
@@ -24,17 +24,30 @@ class Cat:
         eye1.setFillColor("black")
         eye2 = cg.Circle(10, cg.Point(self.center_x + 50, self.center_y - 25))
         eye2.setFillColor("black")
-        ear1 = cg.Polygon(cg.Point(self.center_x - 75, self.center_y - 75), cg.Point(self.center_x - 50, self.center_y - 50), cg.Point(self.center_x - 50, self.center_y - 75))
-        nose = cg.Polygon(cg.Point(self.center_x - 25, self.center_y - 25), cg.Point(self.center_x, self.center_y), cg.Point(self.center_x + 25, self.center_y - 25))
-        nose.setFillColor("yellow")
-        ear1.setDepth(25)
+        ear1 = cg.Polygon(cg.Point(self.center_x + 90, self.center_y - 25), cg.Point(self.center_x + 125, self.center_y - 125), cg.Point(self.center_x + 30, self.center_y - 90))
+        ear2 = cg.Polygon(cg.Point(self.center_x - 90, self.center_y - 25), cg.Point(self.center_x - 125, self.center_y - 125), cg.Point(self.center_x - 30, self.center_y - 90))
+        nose = cg.Polygon(cg.Point(self.center_x - 15, self.center_y), cg.Point(self.center_x, self.center_y + 15), cg.Point(self.center_x + 15, self.center_y))
+        nose_line1 = cg.Path(cg.Point(self.center_x + 15, self.center_y + 30), cg.Point(self.center_x, self.center_y + 15))
+        nose_line2 = cg.Path(cg.Point(self.center_x - 15, self.center_y + 30), cg.Point(self.center_x, self.center_y + 15))
+        nose.setFillColor("pink")
+        ear1.setFillColor(self.color)
+        ear1.setDepth(70)
+        ear2.setFillColor(self.color)
+        ear2.setDepth(70)
         nose.setDepth(25)
+        nose_line1.setBorderWidth(2.5)
+        nose_line2.setBorderWidth(2.5)
+        nose_line1.setDepth(25)
+        nose_line2.setDepth(25)
         paper.add(nose)
-        # paper.add(ear1)
+        paper.add(ear1)
+        paper.add(ear2)
         paper.add(cat)
         paper.add(eye1)
         paper.add(eye2)
+        paper.add(nose_line1)
+        paper.add(nose_line2)
 
 
-fluffy = Cat(300,300, "Blue")
+fluffy = Cat(300,300)
 fluffy.make_cat_appear()
